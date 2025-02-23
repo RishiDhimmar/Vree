7b0519e098f4cf18a0f26b26ec855852fb8eef7b
RishiDhimmar <rishi@hexacoder.com>
2025-02-23 18:16:52 +0530

    Basic Functionalities Done

--- src/components/ColorSelector.jsx
+++ src/components/ColorSelector.jsx
@@ -21,20 +21,22 @@ const ColorSelector = observer(() => {
 
   const [, setSelectedColor] = useState(null);
 
-  // Fix the color click handler
   const handleColorClick = (color) => {
     selectionStore.selectedStuff[selectionStore.selectedPart].color = color;
     setSelectedColor(color);
     selectionStore.setColor(color);
   };
 
+  const handleCustomColorChange = (e) => {
+    const customColor = e.target.value;
+    selectionStore.selectedStuff[selectionStore.selectedPart].color = customColor;
+    setSelectedColor(customColor);
+    selectionStore.setColor(customColor);
+  };
+
   return (
     <div>
-      <div
-        className={`label mt-[20px] ${
-          selectionStore.isDarkTheme ? "" : "text-black"
-        }`}
-      >
+      <div className={`label mt-[20px] ${selectionStore.isDarkTheme ? "" : "text-black"}`}>
         Color
       </div>
       <div className="wrap flex flex-wrap gap-x-15 gap-y-7 mt-3 pl-5">
@@ -43,19 +45,24 @@ const ColorSelector = observer(() => {
             key={index}
             color={color}
             active={
-              color ==
-              selectionStore.selectedStuff[selectionStore.selectedPart].color
+              color === selectionStore.selectedStuff[selectionStore.selectedPart].color
             }
             onClick={() => handleColorClick(color)}
           />
         ))}
-        <CircleContainer
-          path="/assets/texture/custom.png"
-          onClick={() => handleColorClick("/assets/texture/custom.png")}
-        />
+        {/* Custom color container with positioned input */}
+        <div className="relative">
+          <CircleContainer path="/assets/texture/custom.png" />
+          <input
+            id="customColorInput"
+            type="color"
+            className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+            onChange={handleCustomColorChange}
+          />
+        </div>
       </div>
     </div>
   );
-})
+});
 
 export default ColorSelector;
